52ec88ca54c8e2382c7b61d2f4aa668bfaaee30e
HARSH DUBEY <amanad08052@gmail.com>
2024-07-27 15:07:27 +0530

    ui sidebar added

--- invoice-calculator/src/App.js
+++ invoice-calculator/src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Login from './pages/login'
 import Register from './pages/register'
+import Home from './pages/Home'
 
 function App() {
   return (
     <Router>
     <Routes>
-       <Route path="/" element={<Login />} />
+       <Route path="/" element={<Home />} />
        <Route path="/login" element={<Login />} />
        <Route path='/register' element={<Register />} />
     </Routes>
