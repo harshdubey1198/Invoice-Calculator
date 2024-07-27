46235dc73ad64c29041d500444d4ce488cbfb1e3
HARSH DUBEY <amanad08052@gmail.com>
2024-07-27 11:43:17 +0530

    Router Installed

--- invoice-calculator/src/App.js
+++ invoice-calculator/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Login from './Pages/Login'
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+    <Routes>
+       <Route path="/" element={<Login />} />
+  </Routes>
+    </Router>
   )
 }
 
