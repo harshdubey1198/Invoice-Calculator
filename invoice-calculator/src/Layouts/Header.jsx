5e1020d23d5bdb3696baece7fdf27f3334dbb875
HARSH DUBEY <amanad08052@gmail.com>
2024-07-27 15:38:30 +0530

    updated some ui

--- invoice-calculator/src/Layouts/Header.jsx
+++ invoice-calculator/src/Layouts/Header.jsx
@@ -6,34 +6,38 @@ import { FaBars } from 'react-icons/fa'; // Add the hamburger icon
 const Header = ({ onToggleSidebar }) => {
   return (
     <header className="bg-gray-100 text-black flex items-center w-screen h-14 fixed top-0 left-0 z-50 border-b border-black">
-      <div className="container px-10 flex justify-between items-center w-screen">
-        
+      <div className="flex items-center justify-between px-4 w-full">
+        {/* Left Side: Logo */}
         <img 
           src="https://res.cloudinary.com/harshdubey1198/image/upload/v1722064766/logo-light_ipdke7.png" 
           alt="logo" 
           className="w-auto h-9 rounded-lg filter invert" 
         />
-        <nav className="space-x-4">
+        
+        {/* Center: Navigation */}
+        <nav className="hidden md:flex space-x-4">
           <Link to="/" className="hover:text-gray-600">Home</Link>
           <Link to="/proposals" className="hover:text-gray-600">Proposals</Link>
           <Link to="/reports" className="hover:text-gray-600">Reports</Link>
           <Link to="/settings" className="hover:text-gray-600">Settings</Link>
         </nav>
-        <div className='flex gap-5'>
-          <button onClick={onToggleSidebar} className="text-2xl">
-          <FaBars />
-        </button>
+        
+        {/* Right Side: Toggle Sidebar and Profile */}
         <div className="flex items-center space-x-4">
-          <img
-            src="https://res.cloudinary.com/harshdubey1198/image/upload/v1721475588/dummy-userImages/bqof59zlzkampcaxpws9.jpg"
-            alt="profile"
-            className="w-10 h-10 rounded-full"
-          />
-          <div className='flex flex-col'>
-            <h6 className="text-base font-semibold">User Fullname</h6>
-            <p className="text-xs text-gray-600">Admin</p>
+          <button onClick={onToggleSidebar} className="text-2xl">
+            <FaBars />
+          </button>
+          <div className="flex items-center hidden md:flex space-x-4">
+            <img
+              src="https://res.cloudinary.com/harshdubey1198/image/upload/v1721475588/dummy-userImages/bqof59zlzkampcaxpws9.jpg"
+              alt="profile"
+              className="w-10 h-10 rounded-full"
+            />
+            <div className='flex flex-col'>
+              <h6 className="text-base font-semibold">User Fullname</h6>
+              <p className="text-xs text-gray-600">Admin</p>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </header>
