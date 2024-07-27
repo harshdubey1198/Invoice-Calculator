fdb63e5bf3f543563d2fb3b1c6ee5d067c5f2426
rohitsaini27 <rohsaini8@gmail.com>
2024-07-27 17:44:09 +0530

    added validation

--- invoice-calculator/src/App.js
+++ invoice-calculator/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Register from './pages/register'
 import Home from './pages/Home'
 
