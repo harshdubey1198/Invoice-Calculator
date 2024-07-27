814afef5be22eceb78d5163db7efb9089473afd1
HARSH DUBEY <amanad08052@gmail.com>
2024-07-27 14:36:52 +0530

    updated naming conventions

---
+++ invoice-calculator/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Layouts/Header'
+
+function Home() {
+  return (
+    <div>
+        <Header />
+        Home
+        </div>
+  )
+}
+
+export default Home
